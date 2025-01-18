51bbba0598c0b7dac6a72cf34ea376ab224bb289
chrystianruan <chrystianr37@gmail.com>
2025-01-18 12:27:23 -0300

    fix: accessKey verification logic

--- src/main/java/com/api/cauth/config/AuthenticationService.java
+++ src/main/java/com/api/cauth/config/AuthenticationService.java
@@ -16,17 +16,24 @@ public class AuthenticationService {
     public static Authentication getAuthentication(HttpServletRequest request, ProductRepository productRepository) {
         String accessKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
         if (!request.getRequestURL().toString().endsWith("/save-product")) {
-            if (!productRepository.findAll().isEmpty()) {
-                for (Product product : productRepository.findAll()) {
-                    if (accessKey != null && accessKey.equals(product.getAccessKey())) {
-                        break;
-                    }
-                }
+            if (!verifyAccessToken(accessKey, productRepository)) {
+                throw new BadCredentialsException("Invalid accessKey");
             }
-        } else {
-            throw new BadCredentialsException("Invalid Token");
         }
 
         return new ApiKeyAuthentication(accessKey, AuthorityUtils.NO_AUTHORITIES);
+    }
+
+    private static boolean verifyAccessToken(String accessKey, ProductRepository productRepository) {
+        if (!productRepository.findAll().isEmpty()) {
+            for (Product product : productRepository.findAll()) {
+                if (accessKey != null && accessKey.equals(product.getAccessKey())) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
     }
 }
